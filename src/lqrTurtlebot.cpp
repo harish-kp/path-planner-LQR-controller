d129c2b709953c6cf97703d52f610c070a6e05f7
Harish Kp <kperatchi@gmail.com>
2021-08-20 15:27:58 +1000

    Publisher and subscriber update

--- src/lqrTurtlebot.cpp
+++ src/lqrTurtlebot.cpp
@@ -1,7 +1,30 @@
+#include "ros.h"
+#include "stdio.h"
 class lqrController{
     public:
+
+    std::cout << "Create LQR Controller node";
+    std::cout << "..........................";
+    ros::init(argc, argv, "lqrController");
+    ros::NodeHandle n;
+    ros::Publisher vel_pub;
+    vel_pub = nh.advertise<Twist>("/cmd_vel", 10);
+    ros::Subscriber odom_sub;
+    odom_sub = nh.subscribe<Odometry>("/odom", 10, odom_callback);
+    odom_msg = Odometry();
+    pose_msg = Pose();
+    vel_msg = Twist();
+    bool odom_updated = false;
+
+
+    void odom_callback(const Odometry &msg)
+    {
+        odom_msg = msg;
+        odom_updated = true; 
+    }  
+    
     void odom_msg{
-        
+
     }
     int lqrLoop{
 
