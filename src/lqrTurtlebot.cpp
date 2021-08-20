6dded076aba74c7aa417235950e8a76ce18963b7
Harish Kp <kperatchi@gmail.com>
2021-08-20 11:42:55 +1000

    c++ file update

--- src/lqrTurtlebot.cpp
+++ src/lqrTurtlebot.cpp
@@ -1,9 +1,18 @@
 class lqrController{
     public:
+    void odom_msg{
+        
+    }
+    int lqrLoop{
+
+    }
+
 
 }
 int main {
     int T = 150; num_steps = 15000; n = 3; m = 2;p = 3;   
     robot = lqrController();
-    for (int i = 0; i<num_steps)
+    for (int i = 0; i < num_steps; i++){
+        robot.lqrLoop()
+    } 
 }
